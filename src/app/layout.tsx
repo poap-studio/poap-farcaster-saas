00a7d3e5a52fbcf1d28318fe3e1bbc9a3673c462
Alberto G. Toribio <gotoalberto@users.noreply.github.com>
2025-08-25 11:52:30 +0200

    fix: update CSS import path in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-import "~/app/globals.css";
+import "./globals.css";
 import "@farcaster/auth-kit/styles.css";
 import { Providers } from "~/app/providers";
 
